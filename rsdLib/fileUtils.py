2d02e375b2f3ae24153e56d84a817792ad89fdaa
Richard <r.ranson@ieee.org>
2018-08-20 10:38:14 +0100

    added time and freq slice tools

--- rsdLib/fileUtils.py
+++ rsdLib/fileUtils.py
@@ -18,20 +18,8 @@ def s_path():
     """show the seach path used by the shell to find executables"""
     print(os.linesep.join(os.environ['PATH'].split(';')))
 
-## depricated
-##def alterExt(file_name, new_ext, default=False):
-##    """return changed file extension to that given, 
-##       default only if already blank - depricated, see changeExt"""
-##    (root, ext) = path.splitext(file_name)
-##    if (not default) or len(ext)==0 or ext==path.extsep:
-##        if new_ext.find(path.extsep)==0:
-##            ext = new_ext
-##        else:
-##            ext = path.extsep+new_ext
-##    return root+ext
-
 def changeExt(file_name, new_ext, override=False):
-    """return a changed file extension, override True changes any existing ext"""
+    """return a new file.extension, override True to change anyexisting ext"""
     (root, ext) = os.path.splitext(file_name)
     _separator = os.path.extsep
     if override or len(ext)==0 or ext==_separator:
--- rsdLib/sig_math.py
+++ rsdLib/sig_math.py
@@ -21,3 +21,47 @@ def lin(x, power=True):
     else:
         _x = 0.05*numpy.array(x)
     return numpy.power(10, _x)
+
+def __startStop(n, start, stop, span):
+    """start:stop or centre:span index range for an n sample sequence"""
+    # n==0 for 0:len(samples) range 
+    n_start = start if n==0 else int(round(start*n))
+    n_stop = stop if n==0 else int(round(stop*n))
+    if span:
+        h_span = n_stop//2
+        n_stop = n_start + h_span
+        n_start -= h_span
+    if n==0:
+        # linear time sequence
+        return n_start, n_stop
+    else:
+        # offset to line up with freqshift
+        return n_start + n//2, n_stop + n//2
+
+def timeSlice(samples, start, stop, span=False):
+    """return (t, samples[t]) values of a slice of the signal in time,
+       using either start, stop or centre, span"""
+    xx = range(__startStop(0, start, stop, span))
+    return xx, numpy.take(samples, xx)
+
+def freqSlice(samples, start, stop, span=True):
+    """return (f, X[f]) values of a slice of the FFT of the signal in 
+       normalise freq, using either start, stop or centre, span"""
+    n_samp = len(samples)
+    n_start, n_stop = __startStop(n_samp, start, stop, span)
+    # do FFT and shift
+    fr = fftshift(fftfreq(n_samp))
+    X = fftshift(fft(samples))
+    # slice the result
+    return fr[n_start:n_stop], numpy.take(X, range(n_start, n_stop))
+
+def spectSlice(samples, start, stop, span=True, n_fft=512):
+    """return (f, |X[f]|) values of a slice of the spectogram of the signal in
+       normalise freq, using either start, stop or centre, span"""
+    n_start, n_stop = __startStop(n_fft, start, stop, span)
+    # do Spectogram using Welch's method - for now just use the default window
+    X, fr = welch(samples, nfft=n_fft)
+    # re-order, do shift and slice
+    fr = fftshift(fr)
+    X = fftshift(X)
+    return fr[n_start:n_stop], numpy.take(X, range(n_start, n_stop))
